8204b5a38bf1fe4061fdcab748d99f442826d5c9
Ivanyna Dmytro <dimaivanina@MacBook-Pro-Dima.local>
2022-10-11 20:22:57 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/practicWork2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practicWork2.dir/main.cpp.o"
+  "practicWork2"
+  "practicWork2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/practicWork2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/practicWork2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for practicWork2.
+# This may be replaced when dependencies are built.
