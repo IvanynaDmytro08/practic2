0bc516ecf9dd9571e86c44bbfa07e96932c079cc
Ivanyna Dmytro <dimaivanina@MacBook-Pro-Dima.local>
2022-10-11 23:34:14 +0300

    First operator ==

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 
+class Time {
+
+    int m_sec = 0;
+
+public:
+
+    Time(int time) : m_sec(time) {}
+
+
+    friend int operator==(Time &o1, int sec);
+};
+
+
+
+int operator==(Time& o1, int sec){
+
+    return (o1.m_sec == sec);
+}
+
+
+
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+int sec = 1;
+Time time(5);
+
+
+
+
     return 0;
 }
